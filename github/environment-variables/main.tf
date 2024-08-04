20b410e27b6da12d3e0cfda9a6a536de5e41a6ff
Victor Oliveira <victorhugodias2001@gmail.com>
2024-08-03 22:20:29 -0300

    feat(infra): Add Terraform for creating environment variables

---
+++ github/environment-variables/main.tf
@@ -0,0 +1,12 @@
+resource "github_actions_environment_variable" "action_environment_variable" {
+  repository    = var.repository_name
+  environment   = var.environment_name
+
+  for_each = {
+    for index, variables in var.variables :
+    variables.name => variables
+  }
+
+  variable_name = each.value.name
+  value         = each.value.value
+}
